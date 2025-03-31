def8aa45cb3747b0734d86fd1beb9a9155348ef8
Christian Lira <59576606+Christian-Lira@users.noreply.github.com>
2025-03-31 08:03:36 -0700

    Add files via upload

---
+++ Modulo_1_Javascript_Testing/01_sesion/script.js
@@ -0,0 +1,8 @@
+let nombre = "Christian"
+let apellido = "Lopez"
+let nombreCompleto = nombre + " " + apellido;
+
+let edad = 29;
+
+console.log(`Bienvenidos a mi aplicación ${nombreCompleto}`);
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} años`);
